75e2cf3de722c62b34d331285c6a900dea0920c5
jonathanthen <54061710+jonathanthen@users.noreply.github.com>
2019-08-13 19:33:58 +1000

    Add files via upload

---
+++ birthday.py
@@ -0,0 +1,7 @@
+print("Which year were you born?")
+year = input("Year: ")
+year = int(year)
+print("Which birthday number would you like to know?")
+number = input("Number: ")
+number = int(number)
+print(year+number)
---
+++ names.py
@@ -0,0 +1,3 @@
+name = input("What is your first name? ")
+last = input("What is your last name? ")
+print("Your name is: "+ name +" "+last)
