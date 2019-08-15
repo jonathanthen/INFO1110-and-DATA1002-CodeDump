ca331fc6b530b420b158d407f2846724154b882a
Jonathan Then <54061710+jonathanthen@users.noreply.github.com>
2019-08-15 14:15:55 +1000

    Add files via upload

---
+++ compareintandbool.py
@@ -0,0 +1,8 @@
+x = 4
+y = 4
+z = 2
+xySame = (x == y)
+xzSame = (x == z)
+print(("xySame = ") + str(xySame))
+print(("xzSame = ") + str(xzSame))
+print("(xySame == xzSame) = " + str(xySame == xzSame))
---
+++ xyznn.py
@@ -0,0 +1,18 @@
+x = 6
+y = ++x
+z = (x + --y)
+n = 0
+n += 1
+print("x = " + str(x))
+print("y = " + str(y))
+print("z = " + str(z))
+print("n = " + str(n))
+
+x = 5
+r = 1.2
+s = x * r
+print("x = " + str(x))
+print("r = " + str(r))
+print("s = " + str(s))
+s = s - 1.1
+print("s = " + str(s))
