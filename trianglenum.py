6a9a215ae063c7f6033bff2a92194faa7914f0be
Jonathan Then <54061710+jonathanthen@users.noreply.github.com>
2019-08-24 15:44:24 +1000

    Add files via upload

---
+++ trianglenum.py
@@ -0,0 +1,12 @@
+t_num = 1  # the current triangular number to print out 
+step = 2   # the quantity added to `t_num`, to get the next triangular number print('Hello!')
+print('Hello!')
+while True:    
+    if t_num % 2 == 0:        
+        print(t_num)    
+    if t_num % 5 == 0:        
+        print(t_num)
+    if t_num >= 15:        
+        break
+    t_num += step    
+    step += 1
