7d74d25c6b2cbcb6414af46cdc0cb14be1fd98e0
Jonathan Then <54061710+jonathanthen@users.noreply.github.com>
2019-11-14 10:58:35 +1100

    Add files via upload

---
+++ 4bexp1.py
@@ -0,0 +1,14 @@
+for n in range(-2, 2):
+  if n >= 0:
+    print(n, "is positive")
+  else:
+    print(n, "is negative")
+print("For loops are fun")
+
+for n in range(0, 2):
+  n1 = n + 1
+  if n1 >= 5:
+    print(n1, "larger than 5")
+  else:
+    print(n1, "less than 5")
+  print("Next number.")
