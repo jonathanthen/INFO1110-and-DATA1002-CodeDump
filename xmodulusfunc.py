12b35e6c81f4e45fc0ffd9c085102667391a75e5
Jonathan Then <54061710+jonathanthen@users.noreply.github.com>
2020-02-04 15:47:45 +1100

    Add files via upload

---
+++ xmodulusfunc.py
@@ -0,0 +1,33 @@
+def modulus(num,divisor):
+    if type(num) != int or type(divisor) != int:
+        raise TypeError("Input(s) are not integers.")
+    else:
+        # Handle divisor equals to 0 case 
+        if (divisor == 0): 
+            return False
+    
+        n = num
+
+        # Handle negative values 
+        if n < 0:
+            count = 0
+            while n < 0:
+                n += divisor
+                count += 1
+            return n
+        else:
+            count = -1
+            while n > 0:
+                n -= divisor
+                count += 1
+            if n == 0:
+                return 0
+            else:
+                return num - (count * divisor)
+            
+
+print(modulus(16,3)) #1
+print(modulus(-2,3)) #1
+print(modulus(-16,7)) #5
+#print(modulus(True,1))
+print(modulus(10,5))
